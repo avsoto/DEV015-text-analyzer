537c9da8ca32bd0077f97774b34902f600a6983c
avsotomejia@gmail.com <avsotomejia@gmail.com>
2024-07-24 09:25:11 -0500

    Update JS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,25 +1,87 @@
 const analyzer = {
   getWordCount: (text) => {
     const metricItems = document.querySelectorAll('.container ul li');
+    const variosBlancos = /[ ]+/g;
 
-    const wordCount = text.length;
-    metricItems[0].innerHTML = '<li>${</li>'
-    document.getElementById('conteo').innerText = wordCount;
+    text = text.trim();
+
+    text = text.replace(variosBlancos," ");
+
+    const words = text.split(" ");
+    const num = words.length;
+
+    metricItems[4].innerHTML = "Palabras: " + `${num}`;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const metricItems = document.querySelectorAll('.container ul li');
+
+    const wordCount = text.length;
+    metricItems[0].innerHTML = "Caracteres: " + `${wordCount}`;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const metricItems = document.querySelectorAll('.container ul li');
+    const caracteres =  /[^a-zA-Z0-9]/g;
+
+    text = text.replace(/\s+/g, "");
+    text = text.replace(caracteres,"");
+
+    const wordCount = text.length;
+    metricItems[2].innerHTML = "Caracteres sin espacio: " + `${wordCount}`;
   },
+
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const metricItems = document.querySelectorAll('.container ul li');
+    const variosBlancos = /[ ]+/g;
+
+    text = text.trim().replace(variosBlancos, " ");
+
+    const words = text.split(" ");
+    const num = words.length;
+
+    let totalLength = 0;
+
+    for(let i=0;i<words.length;i++){
+      totalLength += words[i].length;
+    }
+
+    const averageWordLength = num > 0 ? (totalLength / num).toFixed(2) : 0;
+
+    metricItems[5].innerHTML = "Promedio longitud: " + `${averageWordLength}`;
+
   },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const metricItems = document.querySelectorAll('.container ul li');
+    const numeros = /[^0-9]/g;
+
+    text = text.replace(numeros,"").length;
+
+    metricItems[1].innerHTML = "Números: " + `${text}`;
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const metricItems = document.querySelectorAll('.container ul li');
+    const numeros = /\d+/g;
+    let suma = 0;
+
+    const numerosEnTexto = text.match(numeros);
+
+    if (numerosEnTexto !== null) {
+      numerosEnTexto.forEach(numeroStr => {
+        const numero = parseInt(numeroStr, 10); // Convertir la cadena a número entero
+        suma =suma + numero; // Sumar el número a 'suma'
+      });
+    }
+
+    metricItems[3].innerHTML = "Suma números: " + `${suma}`;
+
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-document.getElementById('text').addEventListener('input', (event) => {
+document.querySelector("textarea").addEventListener('input', (event) => {
   analyzer.getWordCount(event.target.value);
+  analyzer.getCharacterCount(event.target.value);
+  analyzer.getCharacterCountExcludingSpaces(event.target.value);
+  analyzer.getNumberCount(event.target.value);
+  analyzer.getNumberSum(event.target.value);
+  analyzer.getAverageWordLength(event.target.value);
 });
 
 
 
+document.getElementById("reset-button").addEventListener("click", () => {
+  resetButton();
+});
+
+function resetButton(){
+  const descriptions = [
+    "Caracteres: 0",
+    "Números: 0",
+    "Caracteres sin espacio: 0",
+    "Suma números: 0",
+    "Palabras: 0",
+    "Promedio longitud: 0"
+  ];
+
+  document.querySelector("textarea").value = "";
+  const listItems = document.querySelectorAll(".stats");
+  listItems.forEach((item, index) => {
+    item.textContent = descriptions[index];
+  });
+}
